38b07eae78963751c669a25f850b9b7c12eeb432
Miller Augusto <miller.augusto@gmail.com>
2015-02-02 16:48:18 -0200

    Update ng-ckeditor-1.0.1.js

--- angularjs/plugins/ng-ckeditor/ng-ckeditor-1.0.1.js
+++ angularjs/plugins/ng-ckeditor/ng-ckeditor-1.0.1.js
@@ -6,6 +6,26 @@
 (function(window, document) {
     "use strict";
     (angular.module('ng.ckeditor', [ 'ng' ])).directive('ngCkeditor', function() {
+
+	CKEDITOR.on('instanceCreated', function(event) {
+	    var editor = event.editor, element = editor.element;
+	    if (element.getAttribute('class') == 'simpleEditor') {
+		editor.on('configLoaded', function() {
+		    editor.config.removePlugins = 'colorbutton,find,flash,font, forms,iframe,image,newpage,removeformat, smiley,specialchar,stylescombo,templates';
+		    editor.removeButtons = 'About';
+		    editor.config.toolbarGroups = [ {
+			name : 'editing',
+			groups : [ 'basicstyles', 'links' ]
+		    }, {
+			name : 'undo'
+		    }, {
+			name : 'clipboard',
+			groups : [ 'selection', 'clipboard' ]
+		    } ];
+		});
+	    }
+	});
+
 	var container = function(scope, element, attrs) {
 	    element[0].innerHTML = '<div id="' + attrs.bind + '"></div> <div class="totalTypedCharacters"></div>';
 	    var config = {
@@ -17,12 +37,18 @@
 	    if (attrs.skin != undefined) {
 		config.skin = attrs.skin;
 	    }
-	    (CKEDITOR.appendTo(attrs.bind, config, scope[attrs.bind])).on('change', function(evt) {
-		scope[attrs.bind] = evt.editor.getData();
-		if (attrs.msnCount != undefined) {
-		    element[0].querySelector('.totalTypedCharacters').innerHTML = attrs.msnCount + " " + evt.editor.getData().length;
-		}
-	    });
+	    setTimeout(function() {
+		var editor = CKEDITOR.appendTo(attrs.bind, config, '');
+		(editor).on('change', function(evt) {
+		    eval('(function(){ scope.' + attrs.bind + ' = evt.editor.getData(); })()');
+		    if (attrs.msnCount != undefined) {
+			element[0].querySelector('.totalTypedCharacters').innerHTML = attrs.msnCount + " " + evt.editor.getData().length;
+		    }
+		});
+		scope.$watch(attrs.bind, function(value) {
+		    editor.setData(value);
+		});
+	    }, 500);
 	};
 	return {
 	    restrict : 'E',
