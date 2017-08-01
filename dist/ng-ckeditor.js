4363ec960d24179d58837278c91b3a65eee07000
Ashley Prosser <ashprosser@live.co.uk>
2017-08-01 10:47:59 +0100

    Fix 'Element is not defined' error
    This error occurs when typing in any text or using buttons.
    
    I'm guessing element was changed to elem at some point but the variable name themselves inside the function were not updated.

--- dist/ng-ckeditor.js
+++ dist/ng-ckeditor.js
@@ -69,7 +69,7 @@
                             scope.ngModel = evt.editor.getData();
                         });
                         if (attrs.msnCount != undefined) {
-                            element[0].querySelector('.totalTypedCharacters').innerHTML = attrs.msnCount + " " + evt.editor.getData().length;
+                            elem[0].querySelector('.totalTypedCharacters').innerHTML = attrs.msnCount + " " + evt.editor.getData().length;
                         }
                         if(scope.ngChange && typeof scope.ngChange === 'function'){
                             scope.ngChange(evt.editor.getData());
@@ -84,7 +84,7 @@
 			                    scope.ngModel = evt.editor.getData();
 		                    });
 		                    if (attrs.msnCount != undefined) {
-			                    element[0].querySelector('.totalTypedCharacters').innerHTML = attrs.msnCount + " " + evt.editor.getData().length;
+			                    elem[0].querySelector('.totalTypedCharacters').innerHTML = attrs.msnCount + " " + evt.editor.getData().length;
 		                    }
 		                    if(scope.ngChange && typeof scope.ngChange === 'function'){
 			                    scope.ngChange(evt.editor.getData());
